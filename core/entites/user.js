72a5aa758ffeaede79538203168fe41c996df0a4
nguyennmt3009 <42990346+nguyennmt3009@users.noreply.github.com>
2018-12-19 16:17:03 +0700

    Add new

---
+++ core/entites/user.js
@@ -0,0 +1,14 @@
+
+
+class User {
+    constructor(username, password, fullname, avatar, dob, email, role) { 
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.avatar = avatar;
+        this.dob = dob;
+        this.email = email;
+        this.role = role;
+    }
+}
+
